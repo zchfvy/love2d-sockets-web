09543f404e1f21d0f8ce8980f5f9464656fa4840
Jason Hamilton-Smith <hs.jason@gmail.com>
2017-09-24 23:11:33 -0700

    Got sockets working outside of browser
    Followed the tutorial on website mostly.
    
    Browser version uses websockets correctly but server isn't set up to
    listen on them.

---
+++ build.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+import argparse
+import subprocess
+import os
+import shutil
+import SimpleHTTPServer
+import SocketServer
+import threading
+import time
+import webbrowser
+
+parser = argparse.ArgumentParser(description="Build the Game")
+parser.add_argument('command',
+                    choices=['build', 'clean', 'run'])
+parser.add_argument('--config',
+                    choices=['debug',
+                             'release',
+                             'release-compatibility',
+                             'release-performance'],
+                    default='debug',
+                    help='Configuration to build')
+
+args = parser.parse_args()
+
+if args.config == 'release':
+    args.config = 'release-compatibility'
+
+lovejs = os.environ.get('LOVEJS', os.path.abspath('../love.js/'))
+out_dir = os.path.join('dist', args.config)
+in_dir = 'src'
+
+
+if args.command == 'build':
+    if not os.path.exists(out_dir):
+        orig_path = os.path.join(lovejs, args.config)
+        shutil.copytree(orig_path, out_dir)
+
+    cline = ['python',
+             os.path.join(lovejs, 'emscripten/tools/file_packager.py'),
+             os.path.join(out_dir, 'game.data'),
+             '--preload',
+             os.path.join(in_dir, '@/'),
+             '--js-output=' + os.path.join(out_dir, 'game.js')]
+
+    print('>' + ' '.join(cline))
+
+    subprocess.call(cline)
+
+
+if args.command == 'clean':
+    shutil.rmtree(out_dir)
+
+if args.command == 'run':
+    os.chdir(out_dir)
+
+    Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+    httpd = SocketServer.TCPServer(("", 5000), Handler)
+    print("Serving on port 5000")
+    t = threading.Thread(target=httpd.serve_forever, args=())
+    t.daemon = True
+    t.start()
+
+    webbrowser.open('http://localhost:5000', new=2)
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("\n")
+        print("<Ctrl>+C detected, exiting.")
+        print(" - This may take a few moments: Please Be Patient - ")
+    httpd.shutdown()
+    httpd.server_close()
---
+++ server/server.lua
@@ -0,0 +1,46 @@
+local socket = require("socket")
+local udp = socket.udp()
+
+udp:settimeout(0)
+udp:setsockname('*', 8089)
+
+local world = {}
+local data, msg_or_ip, port_or_nil
+local entity, cmd, arms
+
+local running = true
+
+print "Beginning server loop."
+
+while running do
+    data, msg_or_ip, port_or_nil = udp:receivefrom()
+    if data then
+        entity, cmd, parms = data:match('^(%S*) (%S*) (.*)')
+        if cmd == 'move' then
+            local x, y = parms:match('^(%-?[%d.e]*) (%-?[%d.e]*)$')
+            assert (x and y)
+            x, y = tonumber(x), tonumber(y)
+            local ent = world[entity] or {x=0, y=0}
+            world[entity] = {x=ent.x+x, y=ent.y+y}
+        elseif cmd == 'at' then
+            local x, y = parms:match('^(%-?[%d.e]*) (%-?[%d.e]*)$')
+            assert (x and y)
+            x, y = tonumber(x), tonumber(y)
+            world[entity] = {x=x, y=y}
+        elseif cmd == 'update' then
+            for k, v in pairs(world) do
+                udp:sendto(string.format('%s %s %d %d', k, 'at', v.x, v.y), msg_or_ip, port_or_nil)
+            end
+        elseif cmd == 'quit' then
+            running = false
+        else
+            print('unrecognized command:', cmd)
+        end
+    elseif msg_or_ip ~= 'timeout' then
+        error('Unknown network error: '..tostring(msg_or_ip))
+    end
+
+    socket.sleep(0.01)
+end
+
+print "Thank you."
